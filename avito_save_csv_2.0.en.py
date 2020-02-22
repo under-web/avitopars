2076e0ec5408c19c9ca540484006bea28e6ade75
untitle <53917645+under-web@users.noreply.github.com>
2020-02-22 21:43:38 +0300

    Update avito_save_csv_2.0.en.py

--- avito_save_csv_2.0.en.py
+++ avito_save_csv_2.0.en.py
@@ -4,21 +4,21 @@ import csv
 
 #url = 'https://www.avito.ru/kazan?q=%D0%BC%D1%91%D0%B4' #целевая страница
 
-def get_html(url):                               #функция с именем 'get_html' принимает аргумент 'url'
+def get_html(url):                                   #функция с именем 'get_html' принимает аргумент 'url'
 	r = requests.get(url)                        # переменная 'r ' включает в себя не структурированный текст из 'url' c помощью метода 'get'
 	return r.text                                # и ф-ция возыращает нам переменную 'r' c методом 'text'
 
 def get_total_pages(html):
 	#total_pages = 'https://www.avito.ru/kazan?q=%D0%BC%D1%91%D0%B4&p=5' #функция генерирующая количество (общее) для парсинга страницы
 	total_pages = 5
-	return int(total_pages)
+	return int(total_pages)                      #возвращает общее количество страниц
 
 def write_csv(data):
 	with open('avito.csv', 'a', encoding='utf8') as f:
 		writer = csv.writer(f)
 		writer.writerow((data['title'],
-						data['price'],
-						data['url']))
+				data['price'],
+				data['url']))
 
 def get_page_data(html):
 	soup = BeautifulSoup(html, 'lxml')
@@ -48,12 +48,12 @@ def main():
 	base_url = 'https://www.avito.ru/kazan?q=мёд&'
 	page_part = 'p='
 	
-	total_pages = get_total_pages(get_html(url))
+	total_pages = get_total_pages(get_html(url)) # 
 	
 	for i in range(1, total_pages):
-		url_gen = base_url + page_part + str(i)
-		html = get_html(url_gen)
-		get_page_data(html)
+		url_gen = base_url + page_part + str(i) # | переменная генерирующая различные url в цикле, для дальнейшей с ними работой
+		html = get_html(url_gen)                # \__________В ЭТОМ БЛОКЕ можно добавить возможность вводить вручную через функцию 'input()' 
+		get_page_data(html)                     # /
 		
 		
 
